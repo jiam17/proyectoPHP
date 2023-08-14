bf068b520b0da187383956f4f043c45dca448fba
JRMRIVAS <127703496+JRMRIVAS@users.noreply.github.com>
2023-08-14 10:31:53 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body >
+    <main class="container mt-5 ">
+        <h1 class="text-center text-success">Calculadora Conversor de Unidades</h1>
+        <div class="d-flex justify-content-end mt-5">
+            <div class="dropdown">
+            <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                -- Seleccione la unidad a convertir --
+            </button>
+                <ul class="dropdown-menu border border-primary " >
+                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Datos</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>
+
+                </ul>
+
+            </div>
+        </div>
+
+    </main>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
