60575ddd8774b611147839c5860f8a4651a91956
merymontoya <127900711+merymontoya@users.noreply.github.com>
2023-08-14 15:02:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
                 -- Seleccione la unidad a convertir --
             </button>
                 <ul class="dropdown-menu border border-primary " >
-                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Datos</a></li>
-                    <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
-                    <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./datos.php">Datos</a></li>                    <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
+                    <li><a class="dropdown-item fw-bold " href="./Vista_Longitud.php">Longitud</a></li>
                     <li><a class="dropdown-item fw-bold " href="./VistaMasa.php">Masa</a></li>
                     <li><a class="dropdown-item fw-bold " href="./vistatiempo.php">Tiempo</a></li>
 
